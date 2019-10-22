5a851d027d6e9b37efdf729e6e491eaf114e7111
Hw960828 <Hw960828@users.noreply.github.com>
2019-10-22 23:41:15 +0800

    feat: fistDemo

--- src/main/java/com/cloud/study/netty/firstdemo/TestServer.java
+++ src/main/java/com/cloud/study/netty/firstdemo/TestServer.java
@@ -1,13 +1,25 @@
 package com.cloud.study.netty.firstdemo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class TestServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
 
-
+            ChannelFuture future = serverBootstrap.bind(10010).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
