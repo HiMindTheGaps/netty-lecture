30e6499a370ee2f99976b9a75ebb0ef9ccef1d68
Hw960828 <Hw960828@users.noreply.github.com>
2019-10-22 23:14:21 +0800

    feat: 第一次提交

---
+++ src/main/java/com/cloud/study/netty/firstdemo/TestServer.java
@@ -0,0 +1,13 @@
+package com.cloud.study.netty.firstdemo;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+public class TestServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+    }
+}
